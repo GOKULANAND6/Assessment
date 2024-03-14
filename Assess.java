01b37feb0e0f21a2f7b963ea2cd0f1bb9aa91bd7
GOKULANAND6 <160696205+GOKULANAND6@users.noreply.github.com>
2024-03-14 11:40:33 +0530

    updated by gokul

--- Assess.java
+++ Assess.java
@@ -1,6 +1,12 @@
 class Assess{
+  //insert add
+  //insert subraction
+  //insert multiply
+  //insert divide
   public static void main(String[] args){
     int a = 10;
     int b = 5;
+    float c = 10.0;
+    float d = 5.0;
   }
 }
