fb9a7479008ddde7ba491623a68c2fbdd5edf87d
Gokul Anand <mgokulanand007@gmail.com>
2024-03-14 11:50:57 +0530

    Added by gokulanand

--- Assess.java
+++ Assess.java
@@ -2,11 +2,19 @@ class Assess{
   //insert add
   //insert subraction
   //insert multiply
+  int multiplication(int a, int b{
+	  return (a*b);
+  )
+  float multiplication(float a, float b){
+	  return(a*b);
+  }
   //insert divide
   public static void main(String[] args){
     int a = 10;
     int b = 5;
     float c = 10.0;
     float d = 5.0;
+    System.out.println(multiplication(a, b));
+    System.out.println(multiplication(c, d));
   }
 }
