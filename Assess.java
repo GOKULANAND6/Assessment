ab85d3fa194847491e0c985f2e8f304ec8353cc7
Karthikeyan R K <rkkarthikeyan@gmail.com>
2024-03-14 11:50:54 +0530

    Addtion method is added by karthi

--- Assess.java
+++ Assess.java
@@ -1,5 +1,11 @@
 class Assess{
   //insert add
+  int addition(int a, int b){
+    return a+b;.
+  } 
+  float addition(float a, float b){
+    return a+b;
+  }
   //insert subraction
   //insert multiply
   //insert divide
@@ -8,5 +14,7 @@ class Assess{
     int b = 5;
     float c = 10.0;
     float d = 5.0;
+    System.out.println(additon(a, b));
+    System.out.println(additon(c, d));
   }
 }
