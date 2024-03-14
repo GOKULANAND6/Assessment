bbebe3a05b9e8ba15f0da8fc81da70e8b66f7a1b
Gopikrishnan <gopikrishnanp3007@gmail.com>
2024-03-14 11:49:08 +0530

    The Subraction method added by Gopi

--- Assess.java
+++ Assess.java
@@ -8,5 +8,15 @@ class Assess{
     int b = 5;
     float c = 10.0;
     float d = 5.0;
+    sub(a,b);
+    sub(c,d)
+  }
+  public int sub(a,b){
+	int subr = b-a;
+	return subr;
+  }
+  public float sub(a,b){
+	float subr = b-a;
+	return subr;
   }
 }
