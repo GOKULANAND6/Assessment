57a6dceab50f62ac6c680f4d4835832cd3f204ad
RKKarthikeyan2002 <95752663+RKKarthikeyan2002@users.noreply.github.com>
2024-03-14 11:58:51 +0530

    Code edited by karthi

--- Assess.java
+++ Assess.java
@@ -1,16 +1,6 @@
 class Assess{
   //insert add
   //insert subraction
-  //insert multiply
-  //insert divide
-  public static void main(String[] args){
-    int a = 10;
-    int b = 5;
-    float c = 10.0;
-    float d = 5.0;
-    sub(a,b);
-    sub(c,d)
-  }
   public int sub(a,b){
 	int subr = b-a;
 	return subr;
@@ -18,5 +8,15 @@ class Assess{
   public float sub(a,b){
 	float subr = b-a;
 	return subr;
+  }
+  //insert multiply
+  //insert divide
+  public static void main(String[] args){
+    int a = 10;
+    int b = 5;
+    float c = 10.0;
+    float d = 5.0;
+    System.out.println(sub(a,b));
+    System.out.println(sub(c,d));
   }
 }
