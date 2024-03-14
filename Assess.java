964864b78ffba3348b15beed441163d501786867
Jeevarajan <senorjeeva@gmail.com>
2024-03-14 11:57:25 +0530

    division method included by Jeeva branch

--- Assess.java
+++ Assess.java
@@ -3,10 +3,24 @@ class Assess{
   //insert subraction
   //insert multiply
   //insert divide
+  public static float div(int a, int b){
+	  final float res = a/b;
+	  return res;
+  }
+  //return int value
+  public static float div(float a, float b){
+	  final float res = a/b;
+	  return res;
+  }
+
   public static void main(String[] args){
     int a = 10;
     int b = 5;
     float c = 10.0;
     float d = 5.0;
+
+    //div method calling
+    float res = div(a,b);
+
   }
 }
